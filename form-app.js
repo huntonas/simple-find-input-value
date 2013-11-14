0566f8bba2fa7dc26d65f16da909198b714b7b3e
Andrew Hunton <huntonas@gmail.com>
2013-11-14 13:31:08 -0600

    Add Comments

--- form-app.js
+++ form-app.js
@@ -18,18 +18,23 @@ $(document).ready( function() {
 			form_app_values[i][1] = getInputVals(form_app_values, i);
 		}
 
+		//Insert Calculated value into the 'Total' input field
 		$(form_app_values[5][0]).val(getGTotal(form_app_values));
 
+		//Gets the values in the input fields
 		function getInputVals (form_app_values, i) {
 			 return $(form_app_values[i][0]).val();
 		}
 
+		//Gets the grand total 
 		function getGTotal (form_app_values) {
 			var contTotals = getTotals(form_app_values);
 			form_app_values[5][1] = form_app_values[0][1] - contTotals;
 			return form_app_values[5][1];
 		}
 
+
+		//Adds up all the values in the input fields except the grantTotal
 		function getTotals (form_app_values) {
 			var totals = 0;
 			for(c=1; c < 5; c++) {
